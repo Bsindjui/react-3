2d8f0316f8c7903b7e888ddd3bf896b3e3412155
Blaise7 <blaisearnaud9@gmail.com>
2024-12-11 21:29:06 +0100

    Modified Navbar

--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -3,27 +3,16 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav style={styles.nav}>
-      <ul style={styles.navList}>
+    <nav className="nav">
+      <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/counter">Counter</Link></li>
+        <li><Link to="/color-picker">Color Picker</Link></li>
       </ul>
     </nav>
   );
 };
 
-const styles = {
-  nav: {
-    backgroundColor: '#333',
-    padding: '10px',
-    color: 'white',
-  },
-  navList: {
-    display: 'flex',
-    listStyle: 'none',
-    gap: '15px',
-  },
-};
-
 export default Navbar;
+
